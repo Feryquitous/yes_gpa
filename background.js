7f24579fb411c58379eae750e1287f87517372e5
Mingqian Wu <smilefish64@gmail.com>
2020-03-06 19:27:34 -0600

    Need to work on read

--- background.js
+++ background.js
@@ -14,14 +14,60 @@ firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
 document.addEventListener("DOMContentLoaded", event => {
+    // read data
     const app = firebase.app();
     const db = firebase.firestore();
-    const myEntryRef = db.collection('test_gpa_entries');
+    const myEntryRef = db.collection("test_average_gpa_by_professor_and_class");
+
+    let gpa_list = [];
+
     myEntryRef.get().then(products => {
         products.forEach(doc => {
-            data = doc.data();
-            console.log(data);
-            console.log(data.gpa);
+            gpa_list.push(doc.data());
         })
     });
+
+    console.log(gpa_list);
+
+    // better way to send data?
+    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        if (changeInfo.status == 'complete') {
+            chrome.tabs.query({
+                active: true,
+                currentWindow: true
+            }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, gpa_list);
+            });
+        }
+    });
+
+    // chrome.runtime.onMessage.addListener(function (response, sender, sendResponse) {
+    //     // alert(response);
+    // });
+
+    // write data
+    var ref = db.collection('test_average_gpa_by_professor_and_class').doc('oMLoQReeqcvFlWsrezVC');
+
+    db.runTransaction(transaction => {
+        return transaction.get(ref).then(res => {
+            if (!res.exists) {
+                throw "Document does not exist!";
+            }
+
+            // Compute new number of ratings
+            var newNumRatings = parseInt(res.data().count) + 1;
+
+            // Compute new average rating
+            var oldRatingTotal = res.data().averagegpa * res.data().count;
+            var newAvgRating = (oldRatingTotal + 4) / newNumRatings;
+
+            // Commit to Firestore
+            transaction.update(ref, {
+                count: newNumRatings,
+                averagegpa: newAvgRating
+            });
+
+        })
+    });
+
 });
--- gpa.js
+++ gpa.js
@@ -1,4 +1,11 @@
+// chrome.runtime.sendMessage('Hello');
+
 document.addEventListener("DOMContentLoaded", event => {
+    let avg_gpas = [];
+    chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+        avg_gpas = msg;
+    });
+
     const displayedClasses = document.querySelector('#carousel');
     const classObserverConfig = {
         childList: true,
@@ -7,11 +14,32 @@ document.addEventListener("DOMContentLoaded", event => {
     const classChange = new MutationObserver(mutations => {
         $(".classInstructor").not("[avg-gpa-applied='true']")
             .each(function (i, element) {
-                $(element).text($(element).text() + ' my test GPA');
+                // Extract professor name
+                let professor_name = $(element).text().trim();
 
+                // Extract class ID
+                let class_ID = $(element).parent().parent().first()[0]['innerText'].trim().substring(0, 7);
+
+                let gpa_by_class_professor = 0;
+
+                //check if average gpa present
+                if (avg_gpas.some(avg_gpa => {
+                        if (avg_gpa.professor === professor_name && avg_gpa.classid === class_ID) {
+                            gpa_by_class_professor = avg_gpa.averagegpa;
+                            return true;
+                        } else return false;
+                    })) {
+                    $(element).text($(element).text() + ' ' + gpa_by_class_professor.toString());
+                } else {
+                    $(element).text($(element).text() + ' my test GPA');
+                }
                 // Redundancy Mark  
                 $(element).attr('avg-gpa-applied', 'true');
             });
     });
     classChange.observe(displayedClasses, classObserverConfig);
+
+
+
+
 });
